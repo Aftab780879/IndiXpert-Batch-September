59157d5127d75a0e6327b6bcfcf60cf450b8e8a6
aftab780879 <aftabalam780879@gmail.com>
2025-10-22 23:21:20 +0530

    updated finally

--- Group_Task/project_upgrade.c
+++ Group_Task/project_upgrade.c
@@ -11,15 +11,16 @@ int Number_Entry(int number[],int size)
 }
 int Menu()
 {
-    int choice;
+    int choise;
     printf("\n\n******MENU******\n");
     printf("1.Delete Last Entry\n");
     printf("2.View Records\n");
     printf("3.Insert number\n");
     printf("4.Exit\n");
+    printf("5.Report\n");
     printf("Please Enter Your Choice: ");
-    scanf("%d",&choice);
-    return choice;
+    scanf("%d",&choise);
+    return choise;
 }
 int View_Records(int number[],int size)
 {
@@ -39,7 +40,6 @@ int View_Records(int number[],int size)
 }
 int Insert(int number[],int size)
 {
-   
     if(size<5)
     {
     printf("Please Insert a number: ");
@@ -50,40 +50,100 @@ int Insert(int number[],int size)
     {
         printf("you have already inserted the maximum numbers");
     }
-    return size;
+   return size;
 }
-int main()
+int Report_Menu()
 {
-int number[5];
-int choice;
-int size=5;
-size = Number_Entry(number,size);
+    int report_choise;
+    printf("\n****** Report Menu ******\n");
+    printf("1.Deleated numbers\n");
+    printf("2.Inserted Numbers\n");
+    printf("3.Back\n");
+    printf("please enter your choise: ");
+    scanf("%d",&report_choise);
+    return report_choise; 
+}
+int Report_Operations(int deleted[],int inserted[],int inserted_count,int delete_count)
+{
+    int report_choise;
+    while(1)
+    {
+    report_choise = Report_Menu();
+    if(report_choise==1)
+    {
+        printf("\nDeleted numbers are: ");
+        for(int i=0;i<delete_count;i++)
+        printf("\t%d",deleted[i]);
+    }
+    else if(report_choise==2)
+    {
+        printf("Inserted numbers are: ");
+        for(int i=0;i<inserted_count;i++)
+        printf("\t%d",inserted[i]);
+    }
+    else if(report_choise==3)
+    {
+        break;
+    }
+    else
+    {
+        printf("wrong input");
+    }
+}
+}
+
+int Main_Operations(int choise,int size,int deleted[],int delete_count,int number[],int inserted[],int inserted_count)
+{
 while(1)
 {
-   choice = Menu();
-   if(choice==1)
+   choise = Menu();
+   if(choise==1)
    {
     if(size>0)
     {
+     deleted[delete_count]=number[size-1];
+     delete_count++;
     size--;
     }
    }
-   else if(choice==2)
+   else if(choise==2)
    {
     size=View_Records(number,size);
    }
-   else if(choice==3)
+   else if(choise==3)
    {
     size = Insert(number,size);
+    inserted[inserted_count]=number[size-1];
+    inserted_count++;
    }
-   else if(choice==4)
+   else if(choise==4)
    {
     break;
+   }
+   else if(choise==5)
+   {
+    Report_Operations(deleted,inserted,inserted_count,delete_count);
    }
    else
    {
 printf("Invalid Input");
    }
 }
+}
+
+
+
+int main()
+{
+int number[5];
+int deleted[5];
+int inserted[5];
+int inserted_count=0;
+int delete_count=0;
+int choise;
+int size=5;
+int report_choise;
+size = Number_Entry(number,size);
+Main_Operations(choise,size,deleted,delete_count,number,inserted,inserted_count);
     return 0;
 }
